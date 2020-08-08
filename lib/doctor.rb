33ee382bb4f6790c97a5cea01c904cc35a534bcd
Nicole Stellatos <stellatosnicole@gmail.com>
2020-08-08 02:24:57 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,8 +18,8 @@ class Doctor
   end 
 
   def appointments
-    Appointment.all.select {|appt|
-      appt.doctor == self}
+    Appointment.all.select {|appointment|
+      appointment.doctor == self}
       
 end
 
